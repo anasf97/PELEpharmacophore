6ea8e563aa689bd5ed7ac0f6b2e48f73d8e5f670
anasf97 <anasanchezfernandez1997@gmail.com>
2021-03-02 16:47:42 +0100

    Add github actions for testing

--- PELEpharmacophore/simulate.py
+++ PELEpharmacophore/simulate.py
@@ -1,8 +1,10 @@
-import argparse
+from argparse import ArgumentParser
 from PELEpharmacophore.errors import custom_errors as ce
 from PELEpharmacophore.simulation import docking as dk
 from PELEpharmacophore.simulation import launch_file_creator as lf
 from PELEpharmacophore.simulation import simulation_runner as sr
+import PELEpharmacophore.yaml_parser as yp
+import PELEpharmacophore.valid_flags as vf
 
 def parse_args():
     """
@@ -11,7 +13,7 @@ def parse_args():
     parser = ArgumentParser(description='Run PELEpharmacophore simulations')
     parser.add_argument('input_file', type=str, help='Yaml input file')
     args = parser.parse_args()
-    return args.input_file 
+    return args.input_file
 
 def simulate(target, ligand, center):
     docking = dk.GlideDocking(target, ligand, center)
---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+
+setuptools.setup(name='PELEpharmacophore',
+      version="1.0.0",
+      url = "https://github.com/anasf97/PELEpharmacophore",
+      description='Generate pharmacophore models.',
+      author='Ana Sánchez',
+      author_email='ana.sanchez01@estudiant.upf.edu',
+      packages=setuptools.find_packages(),
+      install_requires=["biopython", "scipy", "dataclasses", "pytest", "flake8"],
+      classifiers=[
+       "Programming Language :: Python :: 3",
+       "License :: OSI Approved :: MIT License",
+       "Operating System :: OS Independent",
+       ],
+     )
