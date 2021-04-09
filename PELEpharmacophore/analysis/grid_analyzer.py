Fix centroid calculation and allow atoms outside the grid

PELEpharmacophore/analysis/grid_analyzer.py
@@ -2,7 +2,6 @@ import os
 import copy
 import numpy as np
 from scipy.spatial import distance
-from functools import reduce
 import PELEpharmacophore.helpers as hl
 import PELEpharmacophore.analysis.grid as gr
 import PELEpharmacophore.analysis.simulation_analyzer as sa
@@ -44,7 +43,7 @@ class GridAnalyzer(sa.SimulationAnalyzer):
         dist = np.sqrt(3)*self.grid.radius #get dist from center to vertex
         atoms_near = hl.neighbor_search(coords, self.grid.center, dist)
         inside_grid_mask = np.logical_and(self.grid.v1 <= atoms_near, atoms_near <=  self.grid.v8).all(axis=1)
-        grid_atoms = coords[inside_grid_mask]
+        grid_atoms = atoms_near[inside_grid_mask]
         return grid_atoms
 
 
@@ -111,19 +110,21 @@ class GridAnalyzer(sa.SimulationAnalyzer):
         """
         coord_dict = super().run(ncpus)
 
-        grid_atoms_dict = {}
-        for feature, coords in coord_dict.items():
-            grid_atoms_dict[feature] = self.get_grid_atoms(coords)
-
         voxel_centers = np.array([v.center for v in self.grid.voxels])
 
-        voxel_ind_dict = {}
-        for feature, coords in grid_atoms_dict.items():
-            voxel_ind_dict[feature] = self.check_voxels(coords, voxel_centers)
+        grid_atoms_dict = {feature: self.get_grid_atoms(coords) \
+                           for feature, coords in coord_dict.items()}
+
+        voxel_ind_dict  = {feature: self.check_voxels(coords, voxel_centers) \
+                           for feature, coords in grid_atoms_dict.items()}
 
         for feature, inds in voxel_ind_dict.items():
             self.fill_grid(feature, inds)
-        print([v.freq_dict for v in self.grid.voxels])
+
+        lst1 = [v.freq_dict for v in self.grid.voxels if v.freq_dict]
+        lst2 = [np.round(np.array([v.center]), decimals=2) for v in self.grid.voxels if v.freq_dict]
+        lst3 = [i for i, v in enumerate(self.grid.voxels) if v.freq_dict]
+        print(list(zip(lst1, lst2, lst3)))
 
     def set_frequency_filter(self, threshold):
         """
@@ -173,7 +174,6 @@ class GridAnalyzer(sa.SimulationAnalyzer):
 
 if __name__ == "__main__":
     target = GridAnalyzer("/home/ana/GitRepositories/PELEpharmacophore/tests/data/simulation_1")
-    #target = GridAnalizer("PELEpharmacophore/1/")
     target.set_ligand("L", "SB2", 800)
     #features={'HBD': ['NC1'], 'HBA': ['NB1', 'NC3', 'O2'], 'ALI': ['FD3', 'C1'], 'ARO': ['CA5', 'CD1']}
     features =  {'HBD': ['NC1'], 'HBA': ['NB1', 'NC3', 'O2'], 'ALI': ['FD3', 'C1'], 'ARO': [('CA1', 'CA4'), ('CD1', 'CD4'), ('CC4', 'CC5', 'CC2')]}

PELEpharmacophore/analysis/simulation_analyzer.py
@@ -22,11 +22,11 @@ class SimulationAnalyzer(metaclass=abc.ABCMeta):
         """
         self.result_dir = f"{indir}/output/"
         self.top_file = os.path.join(self.result_dir, "topologies", "topology_0.pdb")
-        self.trajectories = glob.glob(os.path.join(self.result_dir, "0",  "trajectory_*.pdb"))
-        print(self.trajectories)
+        self.trajectories = glob.glob(os.path.join(self.result_dir, "0",  "trajectory_1.pdb"))
         print(self.top_file)
-        self.reports = glob.glob(os.path.join(self.result_dir, "0", "report_*"))
+        self.reports = glob.glob(os.path.join(self.result_dir, "0", "report_1"))
         self.match_traj_and_report()
+        print(self.traj_and_reports)
         self.chain = None
 
 
@@ -92,7 +92,7 @@ class SimulationAnalyzer(metaclass=abc.ABCMeta):
 
 
 
-    def get_indices(self, topology, resname):
+    def get_indices(self, topology, resname, atomlist):
         """
         Gets all atoms defined in the `features` attribute.
 
@@ -105,56 +105,12 @@ class SimulationAnalyzer(metaclass=abc.ABCMeta):
         ----------
         featured_grid_atoms : list of Atom objects
         """
-        indices_dict = {}
+        indlst  = [hl.get_indices(topology, resname, a) for a in atomlist]
+        indices = np.concatenate([list(i) for i in indlst])
+        lengths = np.array([len(i) for i in indlst])
 
-        for feature, atomlist in self.features.items():
+        return (indices , lengths)
 
-            indlst = [hl.get_indices(topology, resname, a) for a in atomlist]
-            indices = np.concatenate([list(i) for i in indlst])
-            lengths  = np.array([len(i) for i in indlst])
-
-            indices_dict[feature] = (indices , lengths)
-
-        return indices_dict
-
-    @classmethod
-    def get_coordinates(cls, traj_and_report, indices_dict):
-        trajfile, report = traj_and_report
-        print(indices_dict)
-        indices = np.concatenate([i[0] for i in indices_dict.values()])
-        accepted_steps = hl.accepted_pele_steps(report)
-
-        traj = hl.load_trajectory(trajfile, indices)
-        coords = traj.xyz *10  # coord units from nm to A
-        coords = coords[accepted_steps] # duplicate rows when a step is rejected
-
-        coord_dict = {}
-        start = 0
-        for feature, (indices, lengths) in indices_dict.items():
-            print(indices, lengths)
-            stop = start + len(indices)
-            feature_coords = coords[:, start:stop, :].reshape(-1, 3)
-            start = stop
-            feature_coords = cls.calc_cycle_centroids(feature_coords, lengths)
-            coord_dict[feature] = feature_coords
-        return coord_dict
-
-    @classmethod
-    def calc_cycle_centroids(cls, coords, lengths):
-        ind = np.where(lengths > 1)[0]
-        if ind.size == 0:
-            return coords
-
-        acc = list(accumulate(lengths))
-        for i in ind:
-            start = 0 if i == 0 else acc[i-1]
-            stop = acc[i]
-            cycle_coords = coords[start:stop, :]
-            centroid = hl.centroid(cycle_coords)
-            coords[start] = centroid
-            coords[start+1:stop] = np.NaN
-        coords = coords[~ np.all(np.isnan(coords), axis=1)]
-        return coords
 
     @abc.abstractmethod
     def run(self, ncpus):
@@ -164,13 +120,12 @@ class SimulationAnalyzer(metaclass=abc.ABCMeta):
 
         topology = self.get_topology(self.top_file)
 
-        indices_dict = self.get_indices(topology, self.resname)
+        indices_dict = {feature: self.get_indices(topology, self.resname, atomlist) \
+                        for feature, atomlist in self.features.items()}
 
         first_size, first_peak = tracemalloc.get_traced_memory()
 
-        coord_dicts = hl.parallelize(self.__class__.get_coordinates, self.traj_and_reports, ncpus, indices_dict=indices_dict)
-
-        tracemalloc.reset_peak()
+        coord_dicts = hl.parallelize(get_coordinates, self.traj_and_reports, ncpus, indices_dict=indices_dict)
 
         second_size, second_peak = tracemalloc.get_traced_memory()
 
@@ -186,3 +141,38 @@ class SimulationAnalyzer(metaclass=abc.ABCMeta):
     @abc.abstractmethod
     def save_pharmacophores(self):
         pass
+
+def get_coordinates(traj_and_report, indices_dict):
+    trajfile, report = traj_and_report
+    indices = np.concatenate([i[0] for i in indices_dict.values()])
+    accepted_steps = hl.accepted_pele_steps(report)
+
+    traj = hl.load_trajectory(trajfile, indices)
+    coords = traj.xyz *10  # coord units from nm to A
+    coords = coords[accepted_steps] # duplicate rows when a step is rejected
+
+    coord_dict = {}
+    start = 0
+    for feature, (indices, lengths) in indices_dict.items():
+        stop = start + len(indices)
+        feature_coords = coords[:, start:stop, :]
+        start = stop
+        feature_coords = calc_cycle_centroids(feature_coords, lengths)
+        coord_dict[feature] = feature_coords.reshape(-1, 3)
+    return coord_dict
+
+def calc_cycle_centroids(coords, lengths):
+    ind = np.where(lengths > 1)[0]
+    if ind.size == 0:
+        return coords
+
+    acc = list(accumulate(lengths))
+    for i in ind:
+        start = 0 if i == 0 else acc[i-1]
+        stop = acc[i]
+        cycle_coords = coords[:, start:stop, :]
+        centroid = hl.centroid(cycle_coords)
+        coords[:, start, :] = centroid
+        coords[:, start+1:stop, :] = np.NaN
+    coords = coords[~np.all(np.isnan(coords), axis=2)]
+    return coords

PELEpharmacophore/helpers.py
@@ -7,8 +7,10 @@ from sklearn.neighbors import KDTree
 # from Bio.PDB.PDBParser import PDBParser
 # from Bio.PDB.NeighborSearch import NeighborSearch
 
+
 def load_topology(file):
     return md.load(file).topology
+
 
 def get_indices(topology, resname, atoms):
     if isinstance(atoms, list) or isinstance(atoms, tuple):
@@ -16,8 +18,10 @@ def get_indices(topology, resname, atoms):
     query = f"resname {resname} and name {atoms}"
     return topology.select(query)
 
+
 def load_trajectory(file, indices=None):
     return md.load(file, atom_indices=indices)
+
 
 def read_pdb(file):
     parser = PDBParser()
@@ -25,17 +29,19 @@ def read_pdb(file):
     structure = parser.get_structure(pdb_id, file)
     return structure
 
+
 def neighbor_search_biopython(atom_list, center, distance):
     neighbor_search = NeighborSearch(atom_list)
     atoms_near = neighbor_search.search(center, distance, 'A')
     return atoms_near
 
-def neighbor_search(coordinates, center, dist):
+
+def neighbor_search(coords, center, dist):
     center = np.array(center).reshape(1, 3)
-    tree = KDTree(coordinates, leaf_size=3)
+    tree = KDTree(coords)
     result = tree.query_radius(center, r=dist)
     ind = result[0]
-    atoms_near = coordinates[ind]
+    atoms_near = coords[ind]
     return atoms_near
 
 def format_line_pdb(coords, atomname, bfact, models = None, atomnum = "1", resname="UNK", chain="A", resnum="1", occ=1.00):
@@ -45,13 +51,16 @@ def format_line_pdb(coords, atomname, bfact, models = None, atomnum = "1", resna
     line = f"{atomstr:5}{atomnum:>5} {atomname:4} {resname:3} {chain}{resnum:>4}    {x:>8.3f}{y:>8.3f}{z:>8.3f}{occ:6.2f}{bfact:7.2f}{element:>12}{models}\n"
     return line
 
+
 def inside_grid(coord, lower_coord, upper_coord):
     return all(lower_coord <= coord) and all(coord <= upper_coord)
+
 
 def basename_without_extension(filename):
     basename = os.path.basename(filename)
     basename, ext = os.path.splitext(basename)
     return basename
+
 
 def frequency_dict(dict_, key, value):
     if dict_ is None:
@@ -62,6 +71,7 @@ def frequency_dict(dict_, key, value):
         dict_[key] = value
     return dict_
 
+
 def list_dict(dict_, key, value):
     if dict_ is None:
         dict_ = {}
@@ -69,12 +79,16 @@ def list_dict(dict_, key, value):
     return dict_
 
 
+def dict_function(func, dict_):
+    return {key:func(value) for key, value in dict_}
+
+
 def merge_array_dicts(*dicts):
     merged_dict = {}
     union_keys = set().union(*dicts)
 
     for key in union_keys:
-        lst = [i for i, d in enumerate(dicts) if key in d]
+        lst = [i for i, d in enumerate(dicts) if key in d] # ids of the dictionaries where certain key is present
         merged_dict[key] = np.vstack([dicts[i][key] for i in lst])
 
     return merged_dict
@@ -82,6 +96,7 @@ def merge_array_dicts(*dicts):
 
 def custom_path(dir, custom_var, string, ext):
     return os.path.join(dir, f"{custom_var}{string}{ext}")
+
 
 def accepted_pele_steps(report):
     delimiter = " "*4
@@ -102,11 +117,6 @@ def parallelize(func, iterable, n_workers, **kwargs):
     else:
         return list(map(f, iterable))
 
-def centroid(coords):
-    return coords.sum(axis = 0) / len(coords)
 
-def midpoint(point, other_point):
-    x, y, z = point
-    other_x, other_y, other_z = other_point
-    mid = np.array(((x + other_x)/2 , (y + other_y)/2, (z + other_z)/2))
-    return mid
+def centroid(coords):
+    return coords.sum(axis = 1) / coords.shape[1]